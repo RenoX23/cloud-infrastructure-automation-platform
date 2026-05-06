7df813cb352bc613af38e4f3a2841dfee10f15d3
renowiz <whitewolf9181@gmail.com>
2026-05-06 17:27:51 +0000

    Added Terraform VPC infrastructure module

---
+++ terraform/environments/dev/main.tf
@@ -0,0 +1,11 @@
+module "vpc" {
+  source = "../../modules/vpc"
+
+  vpc_name              = "ciap-dev-vpc"
+  vpc_cidr              = var.vpc_cidr
+  public_subnet_1_cidr = var.public_subnet_1_cidr
+  public_subnet_2_cidr = var.public_subnet_2_cidr
+
+  az_1 = var.az_1
+  az_2 = var.az_2
+}
---
+++ terraform/environments/dev/terraform.tfvars
@@ -0,0 +1,7 @@
+vpc_cidr = "10.0.0.0/16"
+
+public_subnet_1_cidr = "10.0.1.0/24"
+public_subnet_2_cidr = "10.0.2.0/24"
+
+az_1 = "ap-south-1a"
+az_2 = "ap-south-1b"
